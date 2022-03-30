c4ff0ed1e9939316cf1780ee0ed34614515467d6
Zoom <k.k.cieslak@wp.pl>
2022-03-30 20:26:35 +0200

    Removed unnecessary compiled class files from repository

--- src/com/jkcieslak/mazegame/AIHandling.java
+++ src/com/jkcieslak/mazegame/AIHandling.java
@@ -13,7 +13,7 @@ public class AIHandling extends Thread{
     public void run() {
         while(SAIO.keepRunning()){
             try{
-                Thread.sleep(1000);
+                Thread.sleep(500);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
